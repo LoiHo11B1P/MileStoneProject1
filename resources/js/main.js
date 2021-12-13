36a73af2784cc50556d90be8ddbbad7871647ff5
Vinh Pham <phamv1@unlv.nevada.edu>
2021-12-13 01:27:18 -0800

    add enemy spawn and colission

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,26 +1,43 @@
 const gameArea = document.querySelector('#game-area');
 const shellCounter = document.querySelector('#shell-count');
-
+const killCounter = document.querySelector('#kill-count');
 // hold attribute relate to the stage of the game
 const gameStage = {
     running: false,
-    leftBoundary: gameArea.clientWidth,
-    rightBoundary: 0,
-    bottomBoundary: gameArea.clientHeight-160,
+    leftBoundary: 0,
+    rightBoundary: gameArea.clientWidth - 10,
+    bottomBoundary: gameArea.clientHeight-40,
     projectiles: [],
     enemies: [],
     spawnEnemyTimer: null,
+    killCount: 0
     
 }
 
-// function gameLoop () {
-//     setInterval(() => {
-
-//     }, 1)
-// }
+function gamgeLoop () {
+    setInterval(() => {
+        if(gameStage.projectiles.length > 0 && gameStage.enemies.length > 0)
+        gameStage.projectiles.forEach((projectile) => {
+            
+            gameStage.enemies.forEach((enemy) => {
+                // colision detection is learn from youtube video and credit to "Franks laboratory"
+                // https://www.youtube.com/watch?v=r0sy-Cr6WHY
+                if(projectile.currentX < enemy.currentX + enemy.width &&
+                    projectile.currentX + projectile.width > enemy.currentX &&
+                    projectile.currentY + enemy.currentY + enemy.height &&
+                    projectile.currentY + projectile.height > enemy.currentY) {
+                        //console.log('HIT!!!!!!!!!')
+                        enemy.die()
+                        gameStage.killCount += 1;
+                    }
+               
+            })
+        }) 
+    }, 1);
+}
 
 function spawnEnemy() {
-    setInterval(() => {
+    return setInterval(() => {
         if(gameStage.running) {
 
             randX = (Math.random() * gameStage.leftBoundary)
@@ -29,25 +46,31 @@ function spawnEnemy() {
             let enemy = new Enemy ({randX, randY});
             enemy.id = randX+'shell'+gameStage.enemies.length
             gameStage.enemies.push(enemy)
-            console.log(gameStage.enemies)
+            killCounter.textContent = `Enemy Killed: ${gameStage.killCount}`
+            //console.log(gameStage.enemies)
         }
-    }, 5000)
+    }, 2500)
 }
 
 // toggle start or pause game
 function startPause(e) {
     gameStage.running = !gameStage.running;
+    let gameLoop = null
     if(!gameStage.running) {
         e.target.style.backgroundColor = 'orange'
+        clearInterval(gameLoop)
     } else {
         e.target.style.backgroundColor ='green'
+        gameLoop = gamgeLoop()
         spawnEnemy()
     }
 }
 
 // position of mouseclick serve as aim point for projectile
 function aimPoint(e) {
-    player.adjustAimPoint(e.clientX, e.clientY)
+    //console.log(e)
+    //player.adjustAimPoint(e.clientX, e.clientY)
+    
 
 }
 
@@ -64,12 +87,14 @@ const player = {
     aimPointX:  0,
     aimPointY:  0,
     positionX: 100,
-    positionY: gameArea.clientHeight - 160,
+    positionY: gameArea.clientHeight - 60,
     prevX: 100,
     img: '',
     playerTank: null,
     tankTurret: null,
-    turretAngle: -45,
+    turretAngle: -10,
+    width: 80,
+    height: 60,
     shellCount: 20,
     maxShell: 25,
     direction: 'right',
@@ -83,6 +108,8 @@ const player = {
         this.playerTank = document.createElement('img')
         this.playerTank.setAttribute('src','resources/media/tank.gif')
         this.playerTank.setAttribute('id', 'player-tank')
+        this.playerTank.style.width = `${this.width}px`
+        this.playerTank.style.height = `${this.height}px`
         this.updateShellCount()
         gameArea.append(this.playerTank)
         gameArea.append(this.tankTurret)
@@ -92,7 +119,15 @@ const player = {
     adjustAimPoint(x,y) {
         this.aimPointX = x;
         this.aimPointY = y;
-        //console.log(`Aiming @ x:${this.aimPointX}, y:${this.aimPointY}`)
+        let distanX = (this.aimPointX - this.currentX)
+        let distanY = (this.aimPointY - this.currentY)
+
+        let angle = Math.atan2(distanY, distanX)
+        
+        this.turretAngle = angle*180/Math.PI
+        this.tankTurret.style.transform = `rotate(${this.turretAngle}deg)`;
+        
+        
     },
 
     tankAction(code) {
@@ -116,9 +151,11 @@ const player = {
     },
 
     updatePosition (x,y) {
-
-        this.positionX = x;
-        this.positionY = y;
+        if(x < gameStage.rightBoundary-10) {
+            this.positionX = x;
+            this.positionY = y;
+       }
+        
         this.moveTank()
         
     },
@@ -135,8 +172,8 @@ const player = {
                 this.tankTurret.style.transform = `rotate(${this.turretAngle}deg)`;
                 this.prevDirection = this.direction
             }
-            this.tankTurret.style.left = `${this.positionX+11}px`;
-            this.tankTurret.style.top = `${this.positionY+5}px`;
+            this.tankTurret.style.left = `${this.positionX+25}px`;
+            this.tankTurret.style.top = `${this.positionY+10}px`;
             
             
         }
@@ -148,8 +185,8 @@ const player = {
                 this.tankTurret.style.transform = `rotate(${this.turretAngle}deg)`;
                 this.prevDirection = this.direction
             }
-            this.tankTurret.style.left = `${this.positionX+25}px`;
-            this.tankTurret.style.top = `${this.positionY+5}px`;
+            this.tankTurret.style.left = `${this.positionX+30}px`;
+            this.tankTurret.style.top = `${this.positionY+10}px`;
         }
 
         if(this.direction === this.prevDirection) {
@@ -192,17 +229,15 @@ gameArea.addEventListener('click', (e) => {
     let newY = 0;
 
     newX = (e.x)
-    newY = (e.y)
-    //console.log(`X:${e.x}, Y:${e.y}`)
+    
     if(gameStage.running) {
         let shell = new Shell({x: player.positionX, y: player.positionY}, {x: e.x, y: e.y})
         player.useShell()
         shell.id = e.x+'shell'+gameStage.projectiles.length
         gameStage.projectiles.push(shell)
-        console.log(gameStage.projectiles)
+        
     }
-    
-    
+
 })
 
 // Projectiles
@@ -213,6 +248,8 @@ class Shell {
     currentX = null
     currentY = null
     speed = 3
+    width = 20
+    height = 20
     id = null
 
     constructor(spawnCoords, explodeCoord) {
@@ -225,9 +262,9 @@ class Shell {
 
         this.shell = document.createElement('img');
         //this.shell.setAttribute('class', 'shell');
-        this.shell.style.width = '20px';
-        this.shell.style.height = '20px';
-        this.shell.style.border = "2px solid gold"
+        this.shell.style.width = `${this.width}px`;
+        this.shell.style.height = `${this.height}px`;
+        this.shell.style.border = "2px solid green"
         this.shell.style.borderRadius = "90%"
         this.shell.style.backgroundColor = 'red';
         this.shell.style.position = 'absolute';
@@ -241,19 +278,18 @@ class Shell {
     }
 
     shellExplode() {
-        this.shell.style.left = `${this.currentX - 200}px`;
-        this.shell.style.top = `${this.currentY - 200}px`;
+        this.width = 100
+        this.height = 100
         this.shell.style.width = '100px'
         this.shell.style.height = '100px'
-        
+        this.shell.style.backgroundColor = 'orange'
         let projectileList = gameStage.projectiles;
         let projectitleIndex = projectileList.indexOf(this.id)
         console.log(gameStage.projectiles[projectitleIndex])
 
         setTimeout(() => {
-           projectileList.splice(projectitleIndex,1)
             
-            
+            projectileList.splice(projectitleIndex,1)
             this.shell.remove()
             console.log(gameStage.projectiles)
         }, 250);
@@ -269,19 +305,15 @@ class Shell {
         let angle = Math.atan2(distanY, distanX)
         
         let aimAngle = angle*180/Math.PI
-        console.log(aimAngle)
+        //console.log(aimAngle)
         
         // if aim angle is not more than -80 or less than -120 do not move the turret or let bullet fly
-        if(aimAngle < 0 && 
-            ((player.direction === 'right' && aimAngle > -80) || (player.direction === 'left' && aimAngle < -120))) {
+        if(aimAngle < 0  && aimAngle > -180) {
             
             player.turretAngle = aimAngle
 
             let verlocityX = Math.cos(angle)*this.speed
             let verlocityY = Math.sin(angle)*this.speed
-    
-            //console.log(`verlocityX: ${verlocityX}`)
-            //console.log(`verlocityY: ${verlocityY}`)
     
             this.shellTravelTimer = setInterval(() => {
     
@@ -300,7 +332,7 @@ class Shell {
             }, 1);
             
         } else {
-            this.shellExplode()
+            this.shell.remove()
         }
 
        
@@ -314,23 +346,35 @@ class Enemy {
     currentY = 0;
     hp = 10;
     id = null;
-    speed = 1;
+    speed = .5;
     soul = null;
+    width = 80;
+    height = 60;
+    travelPath = [
+        [ {x:0, y:100}, {x:400, y:500}, {x:800, y:200},{x: 1000, y: 400} ]
+    ]
 
     constructor(spawnCoords, destCoords, id) {
         this.currentX = spawnCoords.randX;
         this.currentY = spawnCoords.randY;
         this.soul = document.createElement('img')
         this.soul.setAttribute('src', 'resources/media/chopper1.gif')
+        this.soul.style.width = `${this.width}px`
+        this.soul.style.height = `${this.height}px`
         this.soul.style.position = 'absolute';
         gameArea.append(this.soul)
         this.move()
-        this.die()
+        //this.die()
     }
 
-    move () {
+    async move () {
         this.soul.style.left = `${this.currentX}px`;
         this.soul.style.top = `${this.currentY}px`;
+    
+        for (let i = 0; i < this.travelPath[0].length; i++) {
+            await this.Traverse(this.travelPath[0][i])
+        }
+        
     }
 
     die () {
@@ -338,6 +382,51 @@ class Enemy {
             let index = gameStage.enemies.indexOf(this.id)
             gameStage.enemies.splice(index, 1)
             this.soul.remove();
-        }, 3000)
+        }, 50)
+    }
+
+    Traverse (wayPoint) {
+        console.log(wayPoint)
+        new Promise((resolve) => {
+            let toX = wayPoint.x;
+            let toY = wayPoint.y;
+            let distanX = (toX- this.currentX)
+            let distanY = (toY - this.currentY)
+
+            let angle = Math.atan2(distanY, distanX)
+        
+            let aimAngle = angle*180/Math.PI
+
+            if(distanX < 0) {
+                this.soul.style.transform = `rotateY(180deg)`
+            } else {
+                this.soul.style.transform = `rotateY(0deg)`
+            }
+        
+            let verlocityX = Math.cos(angle)*this.speed
+            let verlocityY = Math.sin(angle)*this.speed
+    
+            let flyTime = setInterval(() => {
+                if(gameStage.running) {
+
+                    
+                    this.currentX += verlocityX;
+                    this.currentY += verlocityY;
+        
+                    if((this.currentX * -1 >= this.toX || this.currentX >= gameStage.rightBoundary-10) ||
+                        (this.currentY >= gameStage.bottomBoundary || this.currentY <= this.toY)) {
+                        clearInterval(flyTime);
+                        this.die()
+                    }
+        
+                    this.soul.style.left = `${this.currentX}px`;
+                    this.soul.style.top = `${this.currentY}px`;
+                }
+                    
+            }, 1);
+        })
+        
+            
+      
     }
 }
